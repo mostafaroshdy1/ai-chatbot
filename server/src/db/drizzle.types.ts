9aa8ffb0ebfd25cd45a3051c1da02b7644ad39e0
mostafaroshdy1 <66712535+mostafaroshdy1@users.noreply.github.com>
2025-06-11 23:34:12 +0300

    add chats & chat_messages to schema

--- server/src/db/drizzle.types.ts
+++ server/src/db/drizzle.types.ts
@@ -2,5 +2,13 @@ import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { users } from './schema/users';
 import { aiModels } from './schema/ai-models';
 import { aiModelProviders } from './schema/ai-model-provider';
-export const schema = { users, aiModels, aiModelProviders };
+import { chatMessages } from './schema/chat-messages';
+import { chats } from './schema/chat';
+export const schema = {
+  users,
+  aiModels,
+  aiModelProviders,
+  chatMessages,
+  chats,
+};
 export type Repository = NodePgDatabase<typeof schema>;
