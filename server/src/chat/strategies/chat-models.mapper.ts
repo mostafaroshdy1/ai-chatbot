13e8e51e5749fc9db19c49665bf7d7a0271a297b
mostafaroshdy1 <66712535+mostafaroshdy1@users.noreply.github.com>
2025-06-14 21:36:35 +0300

    feat: add more gemini models

--- server/src/chat/strategies/chat-models.mapper.ts
+++ server/src/chat/strategies/chat-models.mapper.ts
@@ -5,4 +5,8 @@ import { IAiChat } from './IAichat';
 type IAiChatConstructor = new () => IAiChat;
 export const AiChatModelsMapper: Record<AiModel, IAiChatConstructor> = {
   'gemini-1.5-flash': GeminiStrategy,
+  'gemini-1.5-pro': GeminiStrategy,
+  'gemini-1.5-flash-8b': GeminiStrategy,
+  'gemini-2.0-flash-lite': GeminiStrategy,
+  'gemini-2.0-flash': GeminiStrategy,
 };
--- server/src/chat/strategies/models/ai-models.ts
+++ server/src/chat/strategies/models/ai-models.ts
@@ -1,1 +1,6 @@
-export type AiModel = 'gemini-1.5-flash';
+export type AiModel =
+  | 'gemini-1.5-flash'
+  | 'gemini-1.5-pro'
+  | 'gemini-1.5-flash-8b'
+  | 'gemini-2.0-flash-lite'
+  | 'gemini-2.0-flash';
